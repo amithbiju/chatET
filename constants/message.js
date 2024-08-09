bff9a510a067a07dbdcd3cc0e5fb1eed91a1c986
Debilu <114847377+Debilu@users.noreply.github.com>
2024-08-09 16:56:56 +0530

    Update message.js

--- constants/message.js
+++ constants/message.js
@@ -13,9 +13,8 @@ const startmsg =
   "Heya 'user', Welcome to the ChatET experience. Let's do a walkaround shall we 😁.\n\n" +
   "You can always start your conversation with a 'Hi' to see all the important commands used to interact with me 👋.\n\n" +
   "Of course, you can also text other commands if you remember them." +
-  "But just so you do have gold fish memory like me (I forget you guys after you logout ) you can type 'Hi'." +
-
-If you want to see some other commands, type 'help'.
+  "But just so you do have gold fish memory like me (I forget you guys after you logout 😔) you can type 'Hi'.\n\n" +
+  "If you want to see some other commands, type 'help'.\n\n"
 
 You can reach out to my managers and my maker :love: using the 'team' & 'dev' commands respectively. Do let them know your issues and feedbacks. They really love the flattery (Keeps them going :hehe:).
 
