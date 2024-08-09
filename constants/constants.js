0c08c34e35547fac6b4423af5090596c9db4a156
Debilu <114847377+Debilu@users.noreply.github.com>
2024-08-09 13:47:46 +0530

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -19,25 +19,25 @@ You can use these commands to interact with me :winking face:.
 (PS:❗You can use all the commands without '/' prefix too.. :shush:)*/
 
 const himsgout = 
-  "Hello!!! Welcome to ChatET, your personal attendance specialist 😊. How can I assist you today?\n\n" +
+  "*Hello!!!* Welcome to *ChatET*, your personal attendance specialist 😊. How can I assist you today?\n\n" +
   "Here are some commands you can use to interact with me 😉." +
-  "`/login` - To login to ChatET.\n\n" +
-  "`/help` - Get the list of other commands you can use.\n\n" +
-  "`/support` - To get support for your problems.\n\n" +
-  "`/privacy` - Read all the privacy T&C for users 🧐.\n\n" +
+  "`/login` - _To login to ChatET._\n\n" +
+  "`/help` - _Get the list of other commands you can use._\n\n" +
+  "`/support` - _To get support for your problems._\n\n" +
+  "`/privacy` - _Read all the privacy T&C for users 🧐._\n\n" +
   " _(*PS:*❗You can use all the commands without '/' prefix too..🤫)_\n\n" +
   "*Powered by BackTick*";
 
 const helpmsg =
   "Here some other commands you can use...\n\n" +
-  "`/about` - To learn more about me, ChatET 🤗.\n\n" +
-  "`/support` - To get support for your problem.\n\n" +
-  "`/privacy` - Read all the privacy T&C for users 🧐.\n\n" +
-  "`/team` - Meet my Creators & Managers \n\n." +
+  "`/about` - _To learn more about me, ChatET 🤗._\n\n" +
+  "`/support` - _To get support for your problem._\n\n" +
+  "`/privacy` - _Read all the privacy T&C for users 🧐._\n\n" +
+  "`/team` - _Meet my Creators & Managers._ \n\n" +
+  "`/dev` - _Contact of my Maker 🥹._" +
   " _(*PS:*❗You can use all the commands without '/' prefix too..🤫)_\n\n" +
   "*Powered by BackTick*";
 
-/dev - Contact of my Maker :Eyes filled:.
 const privacynote =
   "Privacy Note⭕\n\n" +
   "At ChatET, your privacy and security are our top priorities. We are committed to protecting your personal information and ensuring that your data is handled with the utmost care.\n\n" +
@@ -47,15 +47,15 @@ const privacynote =
   "- Your credentials and attendance information are used solely to provide you with attendance updates and notifications.\n" +
   "- We do not share your personal information with any third parties.\n\n" +
   "*Notifications:*\n" +
-  "- You can choose to enable or disable absence notifications at any time using the commands `/notify-ab` and `/stop-ab`.\n\n" +
+  "- You can choose to enable or disable absence notifications at any time using the commands `/notify` and `/stopab`, respectively.\n\n" +
   "*User Control:*\n" +
-  "- You have full control over your data and can update or delete your credentials at any time using the command `/signin`.\n\n" +
+  "- You have full control over your data and can update or delete your credentials at any time using the command `/logout`.\n\n" +
   "*Security Measures:*\n" +
   "- We employ industry-standard security measures to protect your data from breaches and unauthorized access.\n\n" +
   "*Contact Us:*\n" +
   "- If you have any questions or concerns about your privacy, please contact our support team using the command `/support`.\n\n" +
   "By using ChatET, you agree to our privacy practices as outlined in this note. We are committed to maintaining your trust and protecting your privacy.\n\n" +
-  "*/team* - _The whole team of ChatET._\n*/dev* - _Contact of Developer._";
+  "`/team` - _The whole team of ChatET._\n\n`/dev` - _Contact of Developer._";
 
 const team =
   "Meet our *team BackTick*!!!\n" +
