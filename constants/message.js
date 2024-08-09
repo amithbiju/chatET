05d9c376d99441481d022a4b35c92207d94b694f
Debilu <114847377+Debilu@users.noreply.github.com>
2024-08-09 16:48:46 +0530

    Update message.js

---
+++ constants/message.js
@@ -0,0 +1,10 @@
+const himsgin =
+  "Greetings 'user' 😊. How can I assist you today?\n" +
+  "You can use these commands to interact with me 😉.\n\n" +
+  "/att or /attendance - To retrieve your attendance.\n\n" +
+  "/notify - To enable Absence Detection Notifications.\n\n" +
+  "/stopab - To disable Absence Detection Notifications.\n\n" +
+  "/help - Get the list of other commands you can use.\n\n" +
+  "/support - To get support for your problems.\n\n" +
+  "/logout - To logout of ChatET 😌.\n\n" +
+  "_(*PS*:❗You can use all the commands without '/' prefix too..🤫)_";
