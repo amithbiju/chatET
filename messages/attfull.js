c32ad50008218765aab7b3f25d7d277d16e5a3e1
Amith Biju <amithbiju1@gmail.com>
2024-11-04 01:32:25 +0530

    error

--- messages/attfull.js
+++ messages/attfull.js
@@ -9,44 +9,45 @@ async function attFull(client, MessageMedia) {
 
       const today = new Date();
       let month = today.getMonth() + 1;
+      await msg.reply("Command under maintanance 😓");
 
-      try {
-        const userGet = await getUserData(from);
-        if (userGet) {
-          await msg.reply(
-            `G’day! ${userGet.name}🤗.\nProcessing attendance image, This may take few seconds...`
-          );
-          const imageBase64 = await fetchUserAttendanceImage(
-            userGet.userid,
-            userGet.password
-          );
-          //console.log(imageBase64);
-          const media = new MessageMedia(
-            "image/png",
-            imageBase64,
-            "attendance.png"
-          );
+      // try {
+      //   const userGet = await getUserData(from);
+      //   if (userGet) {
+      //     await msg.reply(
+      //       `G’day! ${userGet.name}🤗.\nProcessing attendance image, This may take few seconds...`
+      //     );
+      //     const imageBase64 = await fetchUserAttendanceImage(
+      //       userGet.userid,
+      //       userGet.password
+      //     );
+      //     //console.log(imageBase64);
+      //     const media = new MessageMedia(
+      //       "image/png",
+      //       imageBase64,
+      //       "attendance.png"
+      //     );
 
-          // Send the image as a WhatsApp message
-          await client
-            .sendMessage(msg.from, media, {
-              caption: "Here is your attendance of the month.",
-            })
-            .then(() => {
-              console.log("test23");
-            });
-        } else {
-          await msg.reply(
-            "Please login to get your attendance !😌. Use `/login`"
-          );
-        }
-      } catch (error) {
-        console.error("Error fetching attendance", error);
-        await client.sendMessage(
-          msg.from,
-          "Sorry, an error has occured in retrieving your attendance image. Please try again later. If this issue persists, please contact `/support`."
-        );
-      }
+      //     // Send the image as a WhatsApp message
+      //     await client
+      //       .sendMessage(msg.from, media, {
+      //         caption: "Here is your attendance of the month.",
+      //       })
+      //       .then(() => {
+      //         console.log("test23");
+      //       });
+      //   } else {
+      //     await msg.reply(
+      //       "Please login to get your attendance !😌. Use `/login`"
+      //     );
+      //   }
+      // } catch (error) {
+      //   console.error("Error fetching attendance", error);
+      //   await client.sendMessage(
+      //     msg.from,
+      //     "Sorry, an error has occured in retrieving your attendance image. Please try again later. If this issue persists, please contact `/support`."
+      //   );
+      // }
     }
   });
 }
